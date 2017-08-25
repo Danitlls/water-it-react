e2323433bd437489b9b1bf4fddcb0ba36dd0a44e
Daniela Trulls <danielatrulls01@gmail.com>
2017-08-25 08:52:53 -0700

    Add initial files and setup.

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from '../styles/App.css';
+
+
+function App(){
+  return (
+    <div>
+      <h1>Welcome to Water It</h1>
+      <p>We will help you stay organized and on time with your plants watering needs.</p>
+    </div>
+  );
+}
+
+export default App;
